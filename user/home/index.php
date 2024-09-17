19b01796d5c5cbe51f30d13b8e8902fc4e5b2444
ADMIN\Admin <cuongkoi1406@gmail.com>
2024-09-17 09:46:48 +0700

    aaa

--- user/home/index.php
+++ user/home/index.php
@@ -1,12 +1,8 @@
 <?php include '../../component/header.php'; ?>
 <?php
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'user') {
-
     header("Location: /no-permission");
     exit();
-}
-if (isset(($_SESSION['token_user']))) {
-    $token_user = $_SESSION['token_user'];
 }
 ?>
 <!DOCTYPE html>
@@ -29,15 +25,18 @@ if (isset(($_SESSION['token_user']))) {
         include '../../component/sidebar.php';
         ?>
         <div class="content_right">
-            <h6>
+            <h6 id="token_display">
                 <?php
-                echo $token_user;
+                if (isset($_SESSION['token_user'])) {
+                    echo $_SESSION['token_user'];
+                }
                 ?>
             </h6>
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
     <script>
         $(document).ready(function () {
             <?php
@@ -46,6 +45,38 @@ if (isset(($_SESSION['token_user']))) {
                 unset($_SESSION['success_login']);
             }
             ?>
+        });
+    </script>
+    <script type="module">
+        import { getTokenFirebase } from '/component/getToken.js'; // Đảm bảo đường dẫn đúng
+
+        $(document).ready(async function () {
+            try {
+                // Lấy token Firebase
+                const token = await getTokenFirebase();
+                if (token) {
+                    console.log('Token:', token);
+
+                    // Gửi token lên server để lưu vào SESSION
+                    const response = await $.ajax({
+                        type: 'POST',
+                        url: 'save_token.php',
+                        data: { token: token },
+                        dataType: 'json'
+                    });
+
+                    if (response.status === 'success') {
+                        // toastr.success('Token đã được lấy và lưu thành công.');
+                        $('#token_display').text(token); // Cập nhật phần tử chứa token
+                    } else {
+                        toastr.error('Không thể lưu token: ' + response.message);
+                    }
+                } else {
+                    toastr.error('Không thể lấy token từ Firebase.');
+                }
+            } catch (error) {
+                toastr.error('Đã xảy ra lỗi trong quá trình lấy token: ' + error.message);
+            }
         });
     </script>
 </body>
